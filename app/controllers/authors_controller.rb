9e402811c3b60337e6a453354eb710aa8ed8f0c4
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-09-21 05:34:08 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,9 +1,4 @@
 class AuthorsController < ApplicationController
-
-  def show
-    @author = Author.find(params[:id])
-  end
-
   def posts_index
   @author = Author.find(params[:id])
   @posts = @author.posts
@@ -18,5 +13,9 @@ def post
   @post = Post.find(params[:post_id])
   render template: 'posts/show'
 end
+
+  def show
+    @author = Author.find(params[:id])
+  end
 
 end
